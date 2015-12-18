10ace5d632582a263d1f040c912ac092a83393d7
jjz <woaf1003@gmail.com>
2015-12-18 10:05:18 +0800

    upgrade

--- meteor-form/meteor-form.js
+++ meteor-form/meteor-form.js
@@ -6,22 +6,39 @@ if (Meteor.isClient) {
 
     Template.body.events({
         'submit .new-language': function (event) {
-            event.preventDefault()
-            var text = event.target.text.value
-            console.log(event)
+            event.preventDefault();
+            var text = event.target.text.value;
+            console.log(event);
             Languages.insert({
                 name: text,
                 createdAt: new Date()
-            })
+            });
             event.target.text.value = ""
         }
     });
+    Template.other_event.helpers({
+        others: Languages.find({}, {limit: 3})
+
+    });
+    Template.other_event.events({
+        'click .delete': function () {
+            Languages.remove(this._id);
+
+        },
+        'click .update': function () {
+            Languages.update(this._id, {
+                $set: {updateAt: new Date()}
+            })
+        }
+
+    });
+
 
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
-       // Languages.removeAllRanges()
+        // Languages.removeAllRanges()
 
     });
 }
