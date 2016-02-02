fa6b49c9e606df3231868caae78cd560db178fff
jjz <woaf1003@gmail.com>
2016-02-02 15:30:16 +0800

    add meteor-security

---
+++ meteor-security/meteor-security.js
@@ -0,0 +1,64 @@
+Meteor.methods({
+    addLanguage: function (text) {
+        //   auth
+        //if (!Meteor.userId()) {
+        //    throw new Meteor.Error("need login");
+        //}
+
+        Languages.insert({
+            name: text,
+            createdAt: new Date()
+        });
+
+
+    },
+    updateLanguage: function (_id) {
+        Languages.update(_id, {
+            $set: {updateAt: new Date()}
+        })
+
+    },
+    removeLanguage: function (_id) {
+        Languages.remove(_id);
+    }
+});
+if (Meteor.isClient) {
+
+    Template.meteor_collection.helpers({
+        languages: Languages.find({}, {sort: {createdAt: -1}})
+    });
+
+    Template.body.events({
+        'submit .new-language': function (event) {
+            event.preventDefault();
+            var text = event.target.text.value;
+            Meteor.call("addLanguage", text);
+
+        }
+    });
+    Template.other_event.helpers({
+        others: Languages.find()
+
+    });
+    Template.other_event.events({
+        'click .delete': function () {
+            Languages.remove(this._id);
+            Meteor.call("removeLanguage", this._id);
+
+        },
+        'click .update': function () {
+            Meteor.call("updateLanguage", this._id);
+        }
+
+    });
+
+
+}
+
+
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        // Languages.removeAllRanges()
+
+    });
+}
